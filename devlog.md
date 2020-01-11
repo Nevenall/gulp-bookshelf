f35103583e28eda908edc3cb22e95f2461bf6493
Dan Behlings <nevenall@hotmail.com>
2020-01-11 09:23:34 -0800

    setting up basic stucture

--- devlog.md
+++ devlog.md
@@ -3,3 +3,11 @@
 ## 2020-11-01
 
 created the initial project. 
+
+first objective is to setup a gulp build pipeline for all the nonsense. 
+does it help to have a static assets folder outside the src structure? 
+
+we would just copy it directly, right?
+more or less, we could always do some extra transformation on the static assets folder
+
+use es2015 modules. 
---
+++ gulpfile.babel.js
@@ -0,0 +1,56 @@
+import { series, parallel, src, dest } from "gulp"
+import { init, write } from "gulp-sourcemaps"
+import babel from "gulp-babel"
+import concat from "gulp-concat"
+import through2 from 'through2'
+
+import {svelte} from 'svelte'
+
+
+// task("default", function () {
+//    return src("src/**/*.js")
+//       .pipe(init())
+//       .pipe(babel())
+//       .pipe(concat("all.js"))
+//       .pipe(write("."))
+//       .pipe(dest("dist"))
+// })
+
+function svelteTask(done) {
+
+   return src("src/App.svelte")
+      .pipe(through2.obj(function (vinyl, _, callback) {
+
+         let ret = svelte.compile(vinyl, {})
+
+      }))
+      .pipe(dest('dist'))
+}
+
+
+/* 
+so, do we run babel first? or on the svelte output? we might have 1 bigish task for building things. 
+becuase we are doing a fair bit of processing . 
+
+So, who does 
+also, not sure I like running svelte on my index page. 
+
+*/
+
+
+function assetsTask(done) {
+   // copy static files
+
+   done()
+}
+
+function cleanTask(done) {
+
+   // clean up the dist directory before we start building
+   done()
+}
+
+let build = series(cleanTask, assetsTask, svelteTask)
+
+
+export default build
