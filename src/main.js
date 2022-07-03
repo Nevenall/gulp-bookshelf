d1554b1c33344fcc02be7dda815da4d714db5b19
nevenall <nevenall@hotmail.com>
2022-07-02 18:39:37 -0700

    working on initial activation

--- src/main.js
+++ src/main.js
@@ -1,22 +1,19 @@
+var refreshing
 if ('serviceWorker' in navigator) {
    navigator.serviceWorker.register('/service-worker.js', { type: 'module' })
       .then(registration => {
          console.log(`main - ready`)
          return navigator.serviceWorker.ready
       })
-      .then(sw => {
-         console.log(`main - activated? = ${sw.active}`)
+      .then(sw => import('/App.svelte'))
+      .then(App => {
          // when the service worker is activated, dynamically import the root svelte component.
          // this prevents the browser from attempting to load files before the service-worker fetch is available
-         import('/App.svelte').then(App => {
-            new App.default({
-               target: document.body,
-               props: {
-                  name: 'world'
-               }
-            })
+         new App.default({
+            target: document.body,
+            props: {
+               name: 'world'
+            }
          })
       })
-
-
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,60 +1,27 @@
-// we are triggering a refresh of tabs when there is a new verison which is ok for us because we don't have data to lose
-var refreshing
-self.addEventListener('controllerchange', event => {
-   console.log('sw controllerchange')
-   if (refreshing) return
-   refreshing = true
-   window.location.reload()
-})
-
 
 
 self.addEventListener('install', event => {
-   console.log('sw install')
+   console.log('sw - install')
    // tell the worker to activate immediately
-   self.skipWaiting()
+   // we don't really have data to be inconsistant
    //todo - fetch and cache pages locally
-   //event.waitUntil(self.skipWaiting())
+   event.waitUntil(self.skipWaiting())
 })
 
 
 
-self.addEventListener('activate',  event => {
-   console.log('sw activated')
-
-   // import('/App.svelte').then(App => {
-   //    new App.default({
-   //       target: document.body,
-   //       props: {
-   //          name: 'world'
-   //       }
-   //    })
-   // })
-
-   // // when the service worker is activated, dynamically import the root svelte component.
-   // // this prevents the browser from attempting to load files before the service-worker fetch is available
-   // const App = await import('/App.svelte')
-
-   // new App.default({
-   //    target: document.body,
-   //    props: {
-   //       name: 'world'
-   //    }
-   // })
-
-
+self.addEventListener('activate', event => {
+   console.log('sw - activated')
 
    // Grab all pages
+   // important for initial installation to work
    event.waitUntil(self.clients.claim())
-
-
 })
-
 
 
 self.addEventListener('fetch', event => {
    // check if requested resource is an import.
-   console.log(event.request.url)
+   console.log(`sw - ${event.request.url}`)
 
    if (event.request.url.endsWith('.svelte')) {
       event.respondWith(fetch(event.request).then(response => {
