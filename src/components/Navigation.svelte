2f9fd3437566a5bcfeaa237480103c46aeeeba06
nevenall <nevenall@hotmail.com>
2023-02-11 17:37:55 -0800

    header titles can be html

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -7,13 +7,13 @@
 <ul>
    {#each chapters as chapter}
       <li class="ripple">
-         <a href={chapter.path}>{chapter.title}</a>
+         <a href={chapter.path}>{@html chapter.title}</a>
          {#if chapter.toc.length > 0 && chapter.toc[0].children.length > 0}
             {@const headers = chapter.toc[0].children}
             <ul>
                {#each headers as h}
                   <li class="ripple">
-                     <a href={chapter.path}{h.url}>{h.title}</a>
+                     <a href={chapter.path}{h.url}>{@html h.title}</a>
                   </li>
                {/each}
             </ul>
